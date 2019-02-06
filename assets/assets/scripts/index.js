6bdc6a04ad14f1c083dd364e7c7c092f20e12cb8
eduruiz <contato@eduruiz.com.br>
2019-02-06 17:41:53 -0200

    Fix JS errors on homepage

--- assets/assets/scripts/index.js
+++ assets/assets/scripts/index.js
@@ -8,11 +8,14 @@ exports.default = backToTop;
 
 function backToTop() {
   const $button = document.querySelector('.js-back-to-top');
-  $button.addEventListener('click', event => {
-    event.preventDefault();
-    const $html = document.querySelector('html');
-    $html.scrollTo(0, 0);
-  });
+
+  if ($button) {
+    $button.addEventListener('click', event => {
+      event.preventDefault();
+      const $html = document.querySelector('html');
+      $html.scrollTo(0, 0);
+    });
+  }
 
   function toggleToTopVisibility() {
     if (window.pageYOffset >= 200) {
@@ -22,8 +25,10 @@ function backToTop() {
     }
   }
 
-  window.addEventListener('scroll', toggleToTopVisibility, false);
-  window.addEventListener('load', toggleToTopVisibility, false);
+  if ($button) {
+    window.addEventListener('scroll', toggleToTopVisibility, false);
+    window.addEventListener('load', toggleToTopVisibility, false);
+  }
 }
 
 },{}],2:[function(require,module,exports){
--- source/scripts/back-to-top/index.js
+++ source/scripts/back-to-top/index.js
@@ -1,11 +1,13 @@
 export default function backToTop() {
   const $button = document.querySelector('.js-back-to-top');
 
-  $button.addEventListener('click', (event) => {
-    event.preventDefault();
-    const $html = document.querySelector('html');
-    $html.scrollTo(0, 0);
-  });
+  if ($button) {
+    $button.addEventListener('click', (event) => {
+      event.preventDefault();
+      const $html = document.querySelector('html');
+      $html.scrollTo(0, 0);
+    });
+  }
 
   function toggleToTopVisibility() {
     if (window.pageYOffset >= 200) {
@@ -15,6 +17,8 @@ export default function backToTop() {
     }
   }
 
-  window.addEventListener('scroll', toggleToTopVisibility, false);
-  window.addEventListener('load', toggleToTopVisibility, false);
+  if ($button) {
+    window.addEventListener('scroll', toggleToTopVisibility, false);
+    window.addEventListener('load', toggleToTopVisibility, false);
+  }
 }
